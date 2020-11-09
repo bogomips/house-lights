c9645d0c6d67c0942020de9a2d0fda58afbdc32b
Pierluigi Petrelli <pierluigi@pepper.com>
2020-11-09 19:53:57 +0100

    adding missing service

---
+++ src/app/services/colors.service.ts
@@ -0,0 +1,81 @@
+import { Injectable } from '@angular/core';
+
+interface RGB {
+  b: number;
+  g: number;
+  r: number;
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ColorsService {
+
+  constructor() { }
+
+  hslToRgb(h,s,l) {
+    // Must be fractions of 1
+    s /= 100;
+    l /= 100;
+  
+    let c = (1 - Math.abs(2 * l - 1)) * s,
+        x = c * (1 - Math.abs((h / 60) % 2 - 1)),
+        m = l - c/2,
+        r = 0,
+        g = 0,
+        b = 0;
+
+    if (0 <= h && h < 60) {
+      r = c; g = x; b = 0;
+    } else if (60 <= h && h < 120) {
+      r = x; g = c; b = 0;
+    } else if (120 <= h && h < 180) {
+      r = 0; g = c; b = x;
+    } else if (180 <= h && h < 240) {
+      r = 0; g = x; b = c;
+    } else if (240 <= h && h < 300) {
+      r = x; g = 0; b = c;
+    } else if (300 <= h && h < 360) {
+      r = c; g = 0; b = x;
+    }
+    r = Math.round((r + m) * 255);
+    g = Math.round((g + m) * 255);
+    b = Math.round((b + m) * 255);
+      
+    return {r:r, g:g, b:b};
+  }
+
+  hslToHex(h, s, l) {
+    h /= 360;
+    s /= 100;
+    l /= 100;
+    let r, g, b;
+    if (s === 0) {
+      r = g = b = l; // achromatic
+    } else {
+      const hue2rgb = (p, q, t) => {
+        if (t < 0) t += 1;
+        if (t > 1) t -= 1;
+        if (t < 1 / 6) return p + (q - p) * 6 * t;
+        if (t < 1 / 2) return q;
+        if (t < 2 / 3) return p + (q - p) * (2 / 3 - t) * 6;
+        return p;
+      };
+      const q = l < 0.5 ? l * (1 + s) : l + s - l * s;
+      const p = 2 * l - q;
+      r = hue2rgb(p, q, h + 1 / 3);
+      g = hue2rgb(p, q, h);
+      b = hue2rgb(p, q, h - 1 / 3);
+    }
+    const toHex = x => {
+      const hex = Math.round(x * 255).toString(16);
+      return hex.length === 1 ? '0' + hex : hex;
+    };
+    return `${toHex(r)}${toHex(g)}${toHex(b)}`;
+  }
+
+  rgbToYIQ({ r, g, b }: RGB): number { 
+    return ((r * 299) + (g * 587) + (b * 114)) / 1000;
+  }
+
+}
